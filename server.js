a79e46abb6fc5ae286a751e946989fbcae7f5782
drcarlosbrunoalencar <carlosbrunoalencar@gmail.com>
2026-06-24 10:12:37 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,14 @@
+{
+  "name": "passometro",
+  "version": "1.0.0",
+  "description": "Passômetro - Gestão de pacientes da enfermaria",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "mongoose": "^8.2.0",
+    "cors": "^2.8.5"
+  }
+}
